cb03648b98736476cb956c309a586ef2c538f201
daisuke-tsunemi <d.tsunemi@soushin-lab.co.jp>
2024-05-17 13:19:04 +0900

    [fix]崩れ修正

--- components/home/fv.js
+++ components/home/fv.js
@@ -2,8 +2,8 @@ import Image from 'next/image';
 export default function Fv() {
   return (
     <section className='p_fv'>
-      <div className="p_fv__head">
-        <div className="u-align--center">
+      <div className="l-container">
+        <div className="p_fv__body">
           <Image
             priority
             src="/images/logo/logo_set.png"
@@ -12,17 +12,13 @@ export default function Fv() {
             alt="Mitsumeru ロゴ"
             className='p_fv__logo'
           />
+          <h2 className="p_fv__head--title c-txt__xl2 c-txt--center">
+              <strong>事業</strong>を<span>見つめる</span><strong>デザイン</strong>を
+          </h2>
+          <h3 className="c-txt__md u-align--center">
+              私たちは、デジタルに強みをもつだけでなく、課題解決、ビジネス変革、そしてサービス開発、あらゆるジャンルのアイデアを統合することで、ブランドと人々の間に強い絆をつくることにチャレンジします。志のあるブランドとともに、そして自らの事業を通じて、マーケットを創造し、文化を発展させ、より多様で豊かな未来を目指します。
+          </h3>
         </div>
-        <h2 className="p_fv__head--title c-txt__xl3 c-txt--center u-w100p">
-            <strong>事業</strong>を<span>見つめる</span><strong>デザイン</strong>を
-        </h2>
-      </div>
-      <div className="p_fv__body">
-      </div>
-      <div className="p_fv__foot">
-        <h3 className="c-txt__md u-align--center">
-            私たちは、デジタルに強みをもつだけでなく、課題解決、ビジネス変革、そしてサービス開発、あらゆるジャンルのアイデアを統合することで、ブランドと人々の間に強い絆をつくることにチャレンジします。志のあるブランドとともに、そして自らの事業を通じて、マーケットを創造し、文化を発展させ、より多様で豊かな未来を目指します。
-        </h3>
       </div>
     </section>
   )
--- pages/layouts/footer.js
+++ pages/layouts/footer.js
@@ -1,25 +1,28 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import Navi from '../layouts/navi';
 
 export default function Footer() {
   return (
     <footer className='l-footer'>
       <div className="p-footer">
-        <div className="p-footer__logo">
-          <Image
-            priority
-            src="/images/logo/logo.png"
-            width={150}
-            height={50}
-            alt="ロゴ"
-          />
+        <div className="p-footer__body">
+          <div className="p-footer__logo">
+            <Image
+              priority
+              src="/images/logo/logo_set.png"
+              width={150}
+              height={50}
+              alt="ロゴ"
+            />
+          </div>
+          <div className="p-footer__menu">
+            <Navi />
+          </div>
         </div>
-        <div className="p-footer__menu">
-          <Navi />
+        <div className="p-footer__foot">
+          <p className="copyright">© Mitsumeru Design Studio All Rights Reserved.</p>
         </div>
       </div>
-      <p className="copyright">1234567890-0iugfcvb</p>
     </footer>
   )
 }
