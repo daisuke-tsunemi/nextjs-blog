90be7a32015e4147c8f8273d3ebac54ba0ab7842
daisuke-tsunemi <d.tsunemi@soushin-lab.co.jp>
2024-05-28 08:22:45 +0900

    [fix]画像変更

--- components/home/about.js
+++ components/home/about.js
@@ -7,30 +7,28 @@ export default function About() {
       <div className='p_section__head'>
         <h2 className='c-txt c-txt--center p_section__title'>About us<span className='c-txt__md'>私たちについて</span></h2>
       </div>
-      <div className='l-container__900'>
+      <div className='l-container__600'>
         <div className='p_section__body'>
           <Image
-            src='/images/home/sample_5.jpg'
-            height={200}
-            width={300}
-            alt="Aboutのイメージ6"
+            src='/images/home/left.webp'
+            height={400}
+            width={600}
+            alt="常見大祐のイメージ1"
             className={styles.p_about__imgLeft}
           />
-          <div className='p_about__text'>
+          <div className={styles.p_about__text}>
             <h3 className='c-txt__lg c-txt--center'>ユーザーを、ビジネスを、ミツメルデザイン</h3>
             <p className='c-txt__md u-mt24'>
-              はじめまして、ミツメルデザインと申します！<br />
-              私たちはWebデザインとグラフィックデザイン開発を通じて、ビジネスの成果を最大化することに情熱を注いでいます。<br />
-              ミツメルデザインのアプローチは、ビジネスの目標や戦略に沿った効果的なデザインを提供することにあります。利用者のニーズを理解し、本当に価値のある体験を提供することで、ビジネスの成功に貢献します。<br />
-              UXデザインにおいては、利用者が直面する課題やニーズを解決するための効果的なデザインを追求します。同時に、メリットだけでなく、潜在的なベネフィットを最大化するためのデザインアプローチを取り入れ、ビジネスの成果を向上させます。<br />
-              ミツメルデザインの使命は、ユーザーとビジネスの両方に焦点を当て、効果的なデザインを提供することです。お困りの際は、ぜひお気軽にご相談ください。ビジネスの価値を共に創り上げましょう。
+              はじめまして、ミツメルデザインのつねみと申します！<br />
+              私たちはWeb、グラフィックデザイン開発を通じて、ビジネスの成果を最大化することに情熱を注いでいます。<br />
+              ミツメルデザインのアプローチは、ビジネスの目標や戦略に沿った効果的なデザインを提供し、ユーザーとビジネスの両方にフォーカス、効果的なデザインを提供することです。<br />お困りの際は、ぜひお気軽にご相談ください。ビジネスの価値を共に創り上げましょう。
             </p>
           </div>
           <Image
-            src='/images/home/sample_6.jpg'
+            src='/images/home/right.webp'
             height={200}
             width={300}
-            alt="Aboutのイメージ6"
+            alt="常見大祐のイメージ2"
             className={styles.p_about__imgRight}
           />
         </div>
